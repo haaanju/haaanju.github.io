cb65de8c977091f2d189092c83229164009de1d3
hanju <me@hanju.kim>
2023-12-12 22:24:53 +0100

    add color name

--- script.js
+++ script.js
@@ -6,16 +6,25 @@ async function fetchColors() {
   displayColors(json.colors);
 }
 
-function displayColors(colors) {
+function displayColors(colors, parentKey = "") {
   for (const key in colors) {
     const color = colors[key];
     if (color.type === "color") {
+      const colorItem = document.createElement("div");
+      colorItem.classList.add("color-item");
+
+      const colorName = document.createElement("span");
+      colorName.textContent = parentKey + key;
+      colorItem.appendChild(colorName);
+
       const colorBox = document.createElement("div");
       colorBox.classList.add("color-box");
       colorBox.style.backgroundColor = color.value;
-      container.appendChild(colorBox);
+      colorItem.appendChild(colorBox);
+
+      container.appendChild(colorItem);
     } else {
-      displayColors(color);
+      displayColors(color, key + " > ");
     }
   }
 }
